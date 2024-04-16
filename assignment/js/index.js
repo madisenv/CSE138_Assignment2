1dd790080e8e38ce7498fafd4848935c37847d94
madisenv <madisen.vong@gmail.com>
2024-04-15 19:25:13 -0700

    Finished step 7 and 8

--- assignment/js/index.js
+++ assignment/js/index.js
@@ -7,7 +7,16 @@ app.data = {
     data: function() {
         return {
             // Complete.
+            input1: null,
+            input2: null,
+            input3: null,
         };
+    },
+    computed: {
+        total: function() {
+            return this.input1 + this.input2 + this.input3
+        }
+
     },
     methods: {
         // Complete.
