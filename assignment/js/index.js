4c5ebd2ed35221f4d336f0a8ce90c33c13bf497d
madisenv <madisen.vong@gmail.com>
2024-04-15 21:08:26 -0700

    Finished line6 computatio (taxable income)

--- assignment/js/index.js
+++ assignment/js/index.js
@@ -2,29 +2,43 @@
 
 let app = {};
 
-
 app.data = {    
     data: function() {
         return {
-            // Complete.
             input1: null,
             input2: null,
             input3: null,
-            checkboxValue: 13850,
+            checkboxVal: 13850,
+            isJoint: false,
+            taxableIncome: 0
         };
     },
     computed: {
         total: function() {
-            return this.input1 + this.input2 + this.input3
+            return this.input1 + this.input2 + this.input3;
+        },
+    },
+    watch: {
+        total() {
+            this.computeTaxableIncome(); 
+        },
+        checkboxVal() {
+            this.computeTaxableIncome();
         }
     },
     methods: {
         checkbox: function() {
-            if (this.isChecked) {
-                this.checkboxValue = 27700
+            if (this.isJoint) {
+                this.checkboxVal = 27700;
+            } else {
+                this.checkboxVal = 13850;
             }
-            else {
-                this.checkboxValue = 13850
+        },
+        computeTaxableIncome: function() {
+            if (this.checkboxVal > this.total) {
+                this.taxableIncome = 0;
+            } else {
+                this.taxableIncome = this.total - this.checkboxVal;
             }
         }
     }
